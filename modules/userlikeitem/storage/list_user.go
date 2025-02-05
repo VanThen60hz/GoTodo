4bed4e12e9b2496e3322e7b7a667375ebdde2b39
VanThen <nguyenvthang2409@gmail.com>
2025-02-06 00:43:08 +0700

    seek paging

--- modules/userlikeitem/storage/list_user.go
+++ modules/userlikeitem/storage/list_user.go
@@ -4,7 +4,11 @@ import (
 	"GoTodo/common"
 	"GoTodo/modules/userlikeitem/model"
 	"context"
+	"github.com/btcsuite/btcutil/base58"
+	"time"
 )
+
+const timeLayout = "2006-01-02T15:04:05.999999"
 
 func (s *sqlStore) ListUsers(
 	ctx context.Context,
@@ -19,9 +23,20 @@ func (s *sqlStore) ListUsers(
 		return nil, common.ErrDB(err)
 	}
 
+	if v := paging.FakeCursor; v != "" {
+		timeCreated, err := time.Parse(timeLayout, string(base58.Decode(v)))
+
+		if err != nil {
+			return nil, common.ErrDB(err)
+		}
+
+		db = db.Where("created_at < ?", timeCreated.Format("2006-01-02 15:04:05.999999"))
+	} else {
+		db = db.Offset((paging.Page - 1) * paging.Limit)
+	}
+
 	if err := db.Select("*").
 		Order("created_at desc").
-		Offset(paging.Page - 1).
 		Limit(paging.Limit).
 		Preload("User").
 		Find(&result).Error; err != nil {
@@ -34,6 +49,11 @@ func (s *sqlStore) ListUsers(
 		users[i] = *result[i].User
 		users[i].UpdatedAt = nil
 		users[i].CreatedAt = result[i].CreatedAt
+	}
+
+	if len(users) > 0 {
+		users[len(result)-1].Mask()
+		paging.NextCursor = base58.Encode([]byte(users[len(result)-1].CreatedAt.Format(timeLayout)))
 	}
 
 	return users, nil
